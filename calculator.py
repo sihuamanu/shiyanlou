58f94ea791c44df71463180a3d89212cf16361e4
Your Name <you@example.com>
2018-05-25 16:21:56 +0800

    123

--- calculator.py
+++ calculator.py
@@ -1,118 +1,178 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 import csv
+import queue
 from multiprocessing import Process, Queue
+from collections import namedtuple
+
+TAX_START_POINT = 3500
+
+Tax_Quick_Lookup_Items = namedtuple(
+    'Quick_Lookup',
+    ['start_point', 'tax_rate', 'quick_subtractor']
+)
+
+INCOME_QUICK_LOOKUP_ITEMS = [
+    Tax_Quick_Lookup_Items(80000, 0.45, 13505),
+    Tax_Quick_Lookup_Items(55000, 0.35, 5505),
+    Tax_Quick_Lookup_Items(35000, 0.30, 2755),
+    Tax_Quick_Lookup_Items(9000, 0.25, 1055),
+    Tax_Quick_Lookup_Items(4500, 0.20, 555),
+    Tax_Quick_Lookup_Items(1500, 0.10, 105),
+    Tax_Quick_Lookup_Items(0, 0.03, 0),
+]
+
+q_user = Queue()
+q_result = Queue()
 
 class Args(object):
-    
     def __init__(self):
         self.args = sys.argv[1:]
-        try:                
-            index = self.args.index('-c')
-            self.configfile = self.args[index+1]
-            index = self.args.index('-d')
-            self.userdata = self.args[index+1]
-            index = self.args.index('-o')
-            self.output = self.args[index+1]
-        except:
-            print("Error")
+
+    def _value_after_option(self, option):
+        try:
+            index = self.args.index(option)
+            return self.args[index + 1]
+        except (ValueError, IndexError):
+            print("Parameter1 Error")
             exit()
 
+    @property
+    def config_path(self):
+        return self._value_after_option('-c')
+
+    @property
+    def userdata_path(self):
+        return self._value_after_option('-d')
+
+    @property
+    def export_path(self):
+        return self._value_after_option('-o')
+
+args = Args()
+
 class Config(object):
-    
+
     def __init__(self):
-        self.config = self._read_config()
+        self.config = self._read_config() 
 
     def _read_config(self):
+        config_path = args.config_path
         config = {}
-        filename = Args().configfile
-        with open(filename) as file:
-            l = file.readlines()
+        with open(config_path) as file:
+            for line in file.readlines():
+                key, value = line.strip().split(' = ')
+                try:
+                    config[key] = float(value)
+                except ValueError:
+                    print("Parameter2 Error")
+                    exit()
+        return config
+
+    def _get_config(self, key):
+        try:
+            return self.config[key]
+        except KeyError:
+            print("KeyError")
+            exit()
+
+    @property
+    def social_insurance_baseline_low(self):
+        return self._get_config('JiShuL')
+
+    @property
+    def social_insurance_baseline_high(self):
+        return self._get_config('JiShuH')
+
+    @property
+    def social_insurance_total_rate(self):
+        return sum([
+            self._get_config('YangLao'),
+            self._get_config('YiLiao'),
+            self._get_config('ShiYe'),
+            self._get_config('GongShang'),
+            self._get_config('ShengYu'),
+            self._get_config('GongJiJin'),
+        ])
+
+config = Config()
+
+class UserData(Process):
+
+    def _read_users_data(self):
+        with open(args.userdata_path) as file:
+            for line in file.readlines():
+                employee_id, income_string = line.strip().split(',')
+                try:
+                    income = int(income_string)
+                except ValueError:
+                    print("Parameter Error")
+                    exit()
+                yield (employee_id, income)
+
+    def run(self):
+        for data in self._read_users_data():
+            q_user.put(data)
+
+class IncomeTaxCalculator(Process):
+
+    @staticmethod
+    def calc_social_insurance_money(income):
+        if income < config.social_insurance_baseline_low:
+            return config.social_insurance_baseline_low * \
+                config.social_insurance_total_rate
+        elif income > config.social_insurance_baseline_high:
+            return config.social_insurance_baseline_high * \
+                config.social_insurance_total_rate
+        else:
+            return income * config.social_insurance_total_rate
+
+    @classmethod
+    def calc_income_tax_and_remain(cls, income):
+        social_insurance_money = cls.calc_social_insurance_money(income)
+        real_income = income - social_insurance_money
+        tax_part = real_income - TAX_START_POINT
+        if tax_part < 0:
+            return '0.00', '{:.2f}'.format(real_income)
+        for item in INCOME_QUICK_LOOKUP_ITEMS:
+            if tax_part > item.start_point:
+                tax = tax_part * item.tax_rate - item.quick_substractor
+                return '{:.2f}'.format(tax), '{:.2f}'.format(real_income - tax)
+
+    def calc_for_all_userdata(self):
+        while True:
             try:
-                for i in l:
-                    a, b = i.split('=')    
-                    config[a.strip()] = float(b)
-            except:
-                print("Error")
-                exit()
-            return config
+                employee_id, income = q_user.get(timeout=1)
+            except queue.Empty:
+                return
+            data = [employee_id, income]
+            social_insurance_money = '{:.2f}'.format(self.calc_social_insurance_money(income))
+            tax, remain = self.calc_income_tax_and_remain(income)
+            data += [social_insurance_money, tax, remain]
+            yield data
 
-class UserData(object):
 
-    def __init__(self):
-        self.userdata = self._read_users_data()
-
-    def _read_users_data(self, queue1):
-        userdata = []
-        filename = Args().userdata
-        with open(filename) as file:
-            l = file.readlines()
-            try:
-                for i in l:
-                    a, b = i.split(',')
-                    userdata.append((a.strip(), int(b)))
-               # return userdata
-                queue1.put(userdata)
-            except:
-                print("Error")
-                exit()
-
-class IncomeTaxCalculator(object):
-
-    def calc_for_all_userdata(self, queue2):
-       # user = UserData().userdata
-        user = queue1.get()
-        config = Config().config
-        coefficient = (config['YangLao'] + config['YiLiao'] + config['ShiYe'] + config['GongShang'] + config['ShengYu'] + config['GongJiJin'])
-        basenum = 3500
-        resultList = []
-        for i in user:
-            if i[1] < config['JiShuL']:
-                insurance = config['JiShuL'] * coefficient  
-            elif i[1] > config['JiShuH']:
-                insurance = config['JiShuH'] * coefficient
-            else:
-                insurance = i[1] * coefficient
-            tax_income = i[1] - insurance - basenum
-            if tax_income <= 0:
-                tax_pay = 0
-            elif tax_income <= 1500:
-                tax_pay = tax_income * 0.03 - 0
-            elif tax_income <= 4500:
-                tax_pay = tax_income * 0.10 - 105
-            elif tax_income <= 9000:
-                tax_pay = tax_income * 0.20 - 555
-            elif tax_income <= 35000:
-                tax_pay = tax_income * 0.25 - 1005
-            elif tax_income <= 55000:
-                tax_pay = tax_income * 0.30 - 2755
-            elif tax_income <= 80000:
-                tax_pay = tax_income * 0.35 - 5505
-            else:
-                tax_pay = tax_income * 0.45 - 13505
-            post_tax_income = i[1] - insurance - tax_pay
-            resultList.append((i[0],i[1],format(insurance, ".2f"),format(tax_pay, ".2f"),format(post_tax_income, ".2f")))
-       # return resultList 
-        queue2.put(resultList)
-
-    def export(self, queue2, default='csv'):
-
-        #result = self.calc_for_all_userdata()
-        result = queue2.get()
-        with open(Args().output, 'w') as file:
-            writer = csv.writer(file)
-            writer.writerows(result)
-
-if __name__ == '__main__':   
+class Exporter(Process):
     
-    queue1 = Queue()
-    queue2 = Queue()
+    def run(self):
+        with open(args.export_path, 'w', newline='') as file:
+            while True:
+                writer = csv.writer(file)
+                try:
+                    item = q_result.get(timeout=1)
+                except queue.Empty:
+                    return
+                writer.writerow(item)
 
-    calpay = IncomeTaxCalculator()
-    #calpay.calc_for_all_userdata()
-    #calpay.export()
-    Process(target=calpay.calc_for_all_userdata, args=(queue1,)).start()
-    Process(target=calpay.calc_for_all_userdata, args=(queue2,)).start()
-    Process(target=calpay.export, args=(queue2,)).start()
+
+if __name__ == '__main__':
+    workers = [
+        UserData(),
+        IncomeTaxCalculator(),
+        Exporter() 
+    ]
+    for worker in workers:
+        worker.run()
+            
+        
